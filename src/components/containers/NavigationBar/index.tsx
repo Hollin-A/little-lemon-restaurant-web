b0aaf4a8037778b059add9b6192420f15d9c73c2
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2025-03-13 21:58:17 +0530

    feat: navigation links added

--- src/components/containers/NavigationBar/index.tsx
+++ src/components/containers/NavigationBar/index.tsx
@@ -1,9 +1,39 @@
-type Props = {}
+import React from "react";
+
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@radix-ui/react-navigation-menu";
+
+const navlinks: { label: string; link: string }[] = [
+  { label: "Home", link: "/home" },
+  { label: "About", link: "/about" },
+  { label: "Menu", link: "/menu" },
+  { label: "Reservations", link: "/reservations" },
+  { label: "Order Online", link: "/order-online" },
+];
+
+type Props = {};
 
 const NavigationBar = (props: Props) => {
   return (
-    <nav>Navigation Bar</nav>
-  )
-}
+    <NavigationMenu>
+      <NavigationMenuList className="flex">
+        {navlinks.map((navlink) => (
+          <NavigationMenuItem key={navlink.link}>
+            <a href={navlink.link}>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                {navlink.label}
+              </NavigationMenuLink>
+            </a>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+};
 
-export default NavigationBar
+export default NavigationBar;
