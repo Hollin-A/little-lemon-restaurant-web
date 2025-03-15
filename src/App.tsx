e0d14fcdf0832e2a8661eb0b98ca439a4b4b1775
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2025-03-15 17:29:56 +0530

    feat:homepage added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import AboutSection from './components/containers/AboutSection'
-import Footer from './components/containers/Footer'
-import HeroSection from './components/containers/HeroSection'
-import Highlights from './components/containers/Highlights'
-import NavigationBar from './components/containers/NavigationBar'
-import Testimonials from './components/containers/Testimonials'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import { ThemeProvider } from './components/theme-provider'
+import { ThemeProvider } from "./components/theme-provider";
 
-import './App.css'
+import HomePage from "./pages/HomePage";
+
+import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>,
+  },
+]);
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <NavigationBar/>
-      <HeroSection/>
-      <Highlights/>
-      <Testimonials/>
-      <AboutSection/>
-      <Footer/>
+      <RouterProvider router={router} />
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,21 @@
+import AboutSection from "@/components/containers/AboutSection";
+import Footer from "@/components/containers/Footer";
+import HeroSection from "@/components/containers/HeroSection";
+import Highlights from "@/components/containers/Highlights";
+import NavigationBar from "@/components/containers/NavigationBar";
+import Testimonials from "@/components/containers/Testimonials";
+
+const HomePage = () => {
+  return (
+    <>
+      <NavigationBar />
+      <HeroSection />
+      <Highlights />
+      <Testimonials />
+      <AboutSection />
+      <Footer />
+    </>
+  );
+};
+
+export default HomePage;
