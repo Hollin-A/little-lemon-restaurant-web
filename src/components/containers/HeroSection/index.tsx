1f5d329256c2894f62433ad72ad69307eed5bbf9
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2025-03-15 09:51:21 +0530

    feat: updates on hero section

--- src/components/containers/HeroSection/index.tsx
+++ src/components/containers/HeroSection/index.tsx
@@ -5,29 +5,36 @@ type Props = {};
 
 const HeroSection = (props: Props) => {
   return (
-    <section className="w-full h-[60vh] flex justify-between items-center container mx-auto gap-40 mt-4">
-      <div className="flex flex-col gap-4 justify-between h-full max-h-[70%]">
-        <div className="flex flex-col gap-2">
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-[#f4ce14]">
-            Little Lemon Restaurant
-          </h1>
-          <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-[#495e57]">
-            Chicago
-          </h4>
-          <p className="leading-7 [&:not(:first-child)]:mt-6">
-            Welcome to Little Lemon, your go-to spot in Chicago for fresh,
-            vibrant flavors and zesty dishes. From cozy dinners to lively
-            gatherings, we craft every meal with love, tradition, and a touch of
-            Mediterranean magic. Join us today!
-          </p>
+    <section className="w-full h-[60vh] mt-4 relative">
+      <div className="w-full h-full flex justify-between items-center container mx-auto gap-40">
+        <div className="flex flex-col gap-4 justify-between h-full max-h-[70%]">
+          <div className="flex flex-col gap-2">
+            <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl text-[#f4ce14]">
+              Little Lemon Restaurant
+            </h1>
+            <h4 className="scroll-m-20 text-xl font-semibold tracking-tight text-[#495e57]">
+              Chicago
+            </h4>
+            <p className="leading-7 [&:not(:first-child)]:mt-6">
+              Welcome to Little Lemon, your go-to spot in Chicago for fresh,
+              vibrant flavors and zesty dishes. From cozy dinners to lively
+              gatherings, we craft every meal with love, tradition, and a touch
+              of Mediterranean magic. Join us today!
+            </p>
+          </div>
+          <Button className="max-w-max">Reserve a table</Button>
         </div>
-        <Button className="max-w-max">Reserve a table</Button>
-      </div>
-      <div className="h-full aspect-square border rounded-xl">
+        <div className="h-[90%] aspect-square border rounded-xl">
+          <img
+            src={heroImage}
+            alt="Hero Image - Restaurant Interior Image"
+            className="w-full h-full object-cover"
+          />
+        </div>
         <img
           src={heroImage}
-          alt="Hero Image - Restaurant Interior Image"
-          className="w-full h-full object-cover"
+          alt="Hero section background"
+          className="absolute w-full h-full -z-10 object-cover top-0 left-0 blur-lg opacity-60"
         />
       </div>
     </section>
