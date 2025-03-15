1ad5c57cbf650135f5c09b1827638cf4357d980b
Hollin Ariyawansa <ariyawansahollin@gmail.com>
2025-03-15 22:42:25 +0530

    feat: basic form with validation

--- src/pages/Reservations/index.tsx
+++ src/pages/Reservations/index.tsx
@@ -1,17 +1,77 @@
 import { useEffect } from "react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 import { fetchAPI } from "@/api/api";
+import PageTitle from "@/components/commons/PageTitle";
 
-type Props = {};
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(2, {
+      message: "Username must be at least 2 characters.",
+    })
+    .max(50),
+});
 
-const ReservationsPage = (props: Props) => {
+const ReservationsPage = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  };
+
   useEffect(() => {
     const response = fetchAPI(new Date("10-10-2025"));
 
     console.log(response);
   }, []);
 
-  return <div>Reservations Page</div>;
+  return (
+    <div className="my-10 container mx-auto">
+      <PageTitle title="Reservations" className="mb-10" />
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Your Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="John Doe" {...field} className="w-1/2" />
+                </FormControl>
+                <FormDescription>
+                  The reservation will be placed under this name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default ReservationsPage;
